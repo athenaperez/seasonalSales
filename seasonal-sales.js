79611e11f759b863e8ac19af1614cbbf2731b98c
Athena Perez <athenaperez@me.com>
2016-12-07 09:11:18 -0600

    displayed departments and products on page via js

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,24 +1,95 @@
+console.log("Seasonal Sales");
+
+// ________________________________________
+// display products
+
+	var data;
+	function getProducts(e) {
+	data = JSON.parse(e.target.responseText)
+	console.log(data)
+
+	var productContent = "";
+	for (var i = 0; i < data.products.length; i ++) {
+		productContent += 	`
+							<div class="idProducts">
+								<h2 id="underline">${data.products[i].name}</h2>
+								<p>${data.products[i].price}</p>
+							</div>
+							`
+	}
+
+	document.getElementById("productID").innerHTML = productContent
+}
+
+
+var userRequest = new XMLHttpRequest()
+	userRequest.addEventListener("load", getProducts)
+	userRequest.open("GET", "products.json")
+	userRequest.send()
+
+// ________________________________________
+// display departments
+
+	var departments;
+
+	function getDepartment(e) {
+	departments = JSON.parse(e.target.responseText)
+	console.log(departments)
+
+
+	var getDepartment = "";
+	for (var i = 0; i < departments.categories.length; i ++) {
+		getDepartment +=	`
+							<div class="idCategories">
+							<h2>${departments.categories[i].name}</h2>
+							`
+	}
+
+	document.getElementById("categoriesID").innerHTML = getDepartment
+}
+
+
+var departmentRequest = new XMLHttpRequest()
+	departmentRequest.addEventListener("load", getDepartment)
+	departmentRequest.open("GET", "departments.json")
+	departmentRequest.send()
+
+
+// ________________________________________
+
+
+	// <p>${data.products[i].category_id}</p>
 
 
 
+// //function
+// 	var user;
+
+// 	function saveUser(e) {
+// 	user = JSON.parse(e.target.responseText)
+// 	console.log("user", user);
+// 	}
+
+// 	function getUser() {
+// 	//create a request
+
+
+// //display the data with a button
 
 
 
-var myRequest = new XMLHttpRequest();
+// document.getElementById("displayUser").addEventListener("click", displayUser)
 
+// function displayUser() {
+// 	console.log("user", user)
+// 	var stuffToDisplay = `
+// 						<h3>${"* " + user.results[0].name.first}
+// 							${user.results[0].name.last + " *"} </h3>
+// 						<img src="${user.results[0].picture.large}">
 
-myRequest.addEventListener("load", function5566 );
-
-
-myRequest.open("GET", "songs.json");
-myRequest.send();
-
-console.log("Last line in JS file");
-console.log(performance.now());
-
-
-
-
+// 						`
+// 	document.getElementById("user").innerHTML = stuffToDisplay
+// }
 
 
 
